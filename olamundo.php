c7718ea5bf640719c6a0382cf9043cfb6354930e
reinaldolvargas <rlvargas@sapo.pt>
2022-04-12 22:14:08 -0300

    primeira alteração

--- olamundo.php
+++ olamundo.php
@@ -9,7 +9,15 @@
 <body>
    <h1>Testando php</h1>
    <?php
-   echo "oi vsmos aprender php";
+   echo"<h2>Ola, mundo!</h2>";
+   echo "<h2><mark>oi vasmos aprender php</mark></h2>";
    ?>
+   <p>A primeira linha 'Testando php' esta em html como titulo 'h1' <br>
+   A segunda linha ja esta dentro da teg de php mesclada com html 'h2' <br>
+   A terceira linha tambem esta em php mesclada com html 'h2' e 'mark' <br>
+   E possivel usar html dentro do codigo php.
+   
+   </p>
+
 </body>
 </html>
---
+++ olamundo1.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+   <meta charset="UTF-8">
+   <meta http-equiv="X-UA-Compatible" content="IE=edge">
+   <meta name="viewport" content="width=device-width, initial-scale=1.0">
+   <title>primeiro exemplo em php</title>
+</head>
+<body>
+   <h1>Testando php</h1>
+   <?php
+    echo "ola mundo"
+   ?>
+</body>
+</html>
