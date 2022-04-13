c00bb5a930e10bba8903567a9bb6174211bfbeaa
reinaldolvargas <rlvargas@sapo.pt>
2022-04-13 16:30:16 -0300

    atualiz1

---
+++ aula004/aula4.php
@@ -0,0 +1,22 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+   <meta charset="UTF-8">
+   <meta http-equiv="X-UA-Compatible" content="IE=edge">
+   <meta name="viewport" content="width=device-width, initial-scale=1.0">
+   <title>Uso de variaveis</title>
+</head>
+<body>
+    <?php
+        $n = 53;
+        $no = " rei ";
+        $na = "rainha"; 
+        echo  $no;
+        echo $n;  
+        
+         echo $no. "  tem  ". $n. " anos  ";
+         echo "$na tem $n anos";
+
+    ?>
+</body>
+</html>
--- olamundo.php
+++ olamundo.php
@@ -12,12 +12,17 @@
    echo"<h2>Ola, mundo!</h2>";
    echo "<h2><mark>oi vasmos aprender php</mark></h2>";
    ?>
-   <p>A primeira linha 'Testando php' esta em html como titulo 'h1' <br>
-   A segunda linha ja esta dentro da teg de php mesclada com html 'h2' <br>
-   A terceira linha tambem esta em php mesclada com html 'h2' e 'mark' <br>
-   E possivel usar html dentro do codigo php.
+   <h1>Super teg para php '&lt;?php---?></h1>
+   <p>E usada para indica que o documento esta sendo escrito em 'php', tudo oque estiver sendo escrito dentro da super teg e interpretado como linguagem php</p>
+   <p>Podemos usar todos os comandos de 'html  e css' dentro de 'php normalmente, o php e um complemento de codigo para o html</p>
+   <h2>comandos em php</h2>
+   <p> > Comando <mark>'echo"---";</mark> Tudo oque estiver dentro das aspas sera mostrado no navegador, para finalizar uma linha de comando e obrigatorio o ':' ponto e virgula</p>
+   <p>A primeira linha 'Testando php' esta em html como titulo 'h1' <br> &lt;h1>Testando php&lt;/h1></p>
+   <p>A segunda linha ja esta dentro da teg de php mesclada com html 'h2' <br> echo"&lt;h2>Ola, mundo!&lt;/h2>";</p>
+   <p>A terceira linha tambem esta em php mesclada com html 'h2' e 'mark' <br> echo "&lt;h2>&lt;mark>oi vasmos aprender php&lt;/mark>&lt;/h2>";</p>
+   <p>E possivel usar html dentro do codigo php.</p>
    
-   </p>
+   
 
 </body>
 </html>
