f70763264d0ba8896883deadb38153e624757b39
reinaldolvargas <rlvargas@sapo.pt>
2022-04-14 15:48:14 -0300

    atual003

--- aula004/aula4.php
+++ aula004/aula4.php
@@ -12,13 +12,21 @@
         $no = " rei ";
         $na = "rainha"; 
         echo  $no;
-        echo $n;  
-        
-         echo $no. "  tem  ". $n. " anos  ";
-         echo 'O codigo e escrito dessa forma $no." tem "." $n. " anos "';
+        echo "$n <br>";  
+        echo "> O codigo e escrito dessa forma <br>";
+         echo '$no. tem . $n.  anos' ; 
+          echo ".> $no.  tem  . $n.  anos <br> "; 
+       
+          echo "forma simplificada <br>";
+         echo'$na tem $n';
+         echo ". > $na tem $n anos <br>
+         > O simbolo de sifrão '$' indica uma variavel <br> 
+         > Os caracteres 'n, no, na são variaveis criadas para indicar idade, rei e rainha, usando essas variaveis na linha  temos a frase que aparece na pagina <br> 
+         > Quando precisamos de espaço entre as variaveis e conteudo usamos o ponto final '.' <br>
+         > Para pular para a proxima linha usamos a teg &lt;br> . dentro das aspas duplas <br>
+         ex:. <br>
+          <mark>  forma simplificada &lt;br>;</mark>; <br>";
          
-         echo "$na tem $n anos";
-
     ?>
 </body>
 </html>
