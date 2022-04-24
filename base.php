0068e8788e0d88e5de776b5816e541c81b48ced7
reinaldolvargas <rlvargas@sapo.pt>
2022-04-24 20:09:51 -0300

    12

--- base.php
+++ base.php
@@ -4,11 +4,44 @@
    <meta charset="UTF-8">
    <meta http-equiv="X-UA-Compatible" content="IE=edge">
    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-   <title></title>
+   <title>repetição2</title>
 </head>
-<h1></h1>
 <body>
+<h1>REPETIÇÃO COM TESTE LOGICO NO FINAL (do)</h1>
+<p>Ao inves de comessar com 'while' começa com 'do'<br>
+variavel = valor de inicio;<br>
+do{<br>
+echo variavel (informações de leitura)
+$c++; variavel de contagem<br>
+while(variavel<= valor final)</p>
+<p>Example :<br>
+ 24 April, 2022 contagem 1, 2, 3, 4, 5, 6, 7, 8, 9, 10,</p>
+<p>&#36c=1;
+echo date("d F, Y");
+echo " contagem  ";
+do{
+echo "&#36c,";
+
+&#36c++;
+} while(&#36c<=10)</p>
 <?php
+$c=1;
+echo date("d F, Y");
+echo " contagem  ";
+do{
+echo "$c,";
+$c++;
+} while($c<=10)
+?>
+<p>echo"Example contagem regressiva:<br /></p>
+<?php
+$c1=10;
+echo date("d F, Y");
+echo " contagem  ";
+do{
+echo "$c1,";
+$c1--;
+} while($c1>=1)
 ?>
 </body>
 </html>
